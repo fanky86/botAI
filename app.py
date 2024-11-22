ff792fe8ad28b58630415b758ee58edae794ed6e
Tanpa Nama <106883384+fanky86@users.noreply.github.com>
2024-11-22 12:25:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from twilio.rest import Client
 
 # Masukkan SID dan Auth Token yang didapat
 account_sid = 'AC32ada6b57607523b956b175776dffa48'  # Gantilah dengan Account SID Anda
-auth_token = 't3JBo1VLsNIVhQ7alfa0t9RSI2Hy6eLA'  # Gantilah dengan Auth Token Anda
+auth_token = 'a8a968dba53f504a89d1c6ed4914e22a'  # Gantilah dengan Auth Token Anda
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body='Hello, this is a test message from Twilio WhatsApp bot!',
     from_='whatsapp:+14155238886',  # Nomor WhatsApp Sandbox Anda
-    to='whatsapp:+628978825442'  # Nomor WhatsApp tujuan
+    to='whatsapp:+62895359611122'  # Nomor WhatsApp tujuan
 )
 
 print(f"Message SID: {message.sid}")
